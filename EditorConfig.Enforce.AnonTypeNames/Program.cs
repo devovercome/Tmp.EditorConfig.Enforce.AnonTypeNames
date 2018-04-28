e89de7a9894eb5ccee4de0db5f503bc9f684f743
alex <thruthseekers@mail.ru>
2018-04-28 20:56:14 +0300

    init

---
+++ EditorConfig.Enforce.AnonTypeNames/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace EditorConfig.Enforce.AnonTypeNames
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var dateTime = System.DateTime.Now;
+			// dotnet_style_prefer_inferred_anonymous_type_member_names = false:error
+			var foo = new
+			{
+				Second = dateTime.Second, // muted this suggestion, but
+				dateTime.Month // this should raise error.
+			};
+		}
+	}
+}
